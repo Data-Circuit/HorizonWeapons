602118f0295384e6b509e7f8ae9e940311b63808
olliejohnson <oliver.joseph.johnson@gmail.com>
2026-05-14 09:00:28 -0400

    Increased slowdown speed for anchor effect

--- src/main/java/io/github/datacircuit/horizonweapons/effect/AnchorEffect.java
+++ src/main/java/io/github/datacircuit/horizonweapons/effect/AnchorEffect.java
@@ -5,6 +5,7 @@ import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.phys.Vec3;
 import org.jspecify.annotations.NonNull;
 
@@ -22,7 +23,7 @@ public class AnchorEffect extends MobEffect {
     public boolean applyEffectTick(@NonNull ServerLevel serverLevel, @NonNull LivingEntity mob, int amplification) {
         if (mob instanceof ServerPlayer sp && sp.isFallFlying()) {
             Vec3 velocity = sp.getDeltaMovement();
-            Vec3 newVelocity = velocity.scale(1.0 - .1f);
+            Vec3 newVelocity = velocity.scale(1.0 - .2f);
             sp.setDeltaMovement(newVelocity);
             sp.hurtMarked = true;
         }
