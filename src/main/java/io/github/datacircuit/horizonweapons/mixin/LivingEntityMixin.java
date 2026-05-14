d9f910c1be1750e341be2ecd3180606f920ea17d
olliejohnson <oliver.joseph.johnson@gmail.com>
2026-05-13 20:39:15 -0400

    Anchor Effect

--- src/main/java/io/github/datacircuit/horizonweapons/mixin/LivingEntityMixin.java
+++ src/main/java/io/github/datacircuit/horizonweapons/mixin/LivingEntityMixin.java
@@ -5,15 +5,12 @@ import net.minecraft.core.Holder;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.effect.MobEffect;
-import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
-import java.util.Collection;
 
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin {
--- src/main/java/io/github/datacircuit/horizonweapons/registry/HorizonWeaponsEffects.java
+++ src/main/java/io/github/datacircuit/horizonweapons/registry/HorizonWeaponsEffects.java
@@ -1,6 +1,7 @@
 package io.github.datacircuit.horizonweapons.registry;
 
 import io.github.datacircuit.horizonweapons.HorizonWeapons;
+import io.github.datacircuit.horizonweapons.effect.AnchorEffect;
 import io.github.datacircuit.horizonweapons.effect.ConfusionEffect;
 import io.github.datacircuit.horizonweapons.effect.VitalityEffect;
 import net.minecraft.core.Holder;
@@ -16,6 +17,9 @@ public class HorizonWeaponsEffects {
     public static final Holder<MobEffect> CONFUSION =
             Registry.registerForHolder(BuiltInRegistries.MOB_EFFECT, Identifier.fromNamespaceAndPath(HorizonWeapons.MOD_ID, "confusion"),
                     new ConfusionEffect());
+    public static final Holder<MobEffect> ANCHOR =
+            Registry.registerForHolder(BuiltInRegistries.MOB_EFFECT, Identifier.fromNamespaceAndPath(HorizonWeapons.MOD_ID, "anchor"),
+                    new AnchorEffect());
 
     public static void init() {}
 }
